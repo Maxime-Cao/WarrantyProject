d58aa127953e24045ed1f0824ae2a686eb9d861d
Tom C <31369826+tomcauf@users.noreply.github.com>
2023-03-01 14:05:44 +0100

    Update

--- app/src/main/java/com/warranty/warrantyproject/MainActivity.kt
+++ app/src/main/java/com/warranty/warrantyproject/MainActivity.kt
@@ -2,10 +2,12 @@ package com.warranty.warrantyproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
