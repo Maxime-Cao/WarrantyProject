3cb787d17a100db814bf720a87b95c1c660f01d8
Tom C <31369826+tomcauf@users.noreply.github.com>
2023-03-04 20:34:45 +0100

    rework fragment

--- app/src/main/java/com/warranty/warrantyproject/AddFragment.kt
+++ app/src/main/java/com/warranty/warrantyproject/AddFragment.kt
@@ -1,12 +1,20 @@
 package com.warranty.warrantyproject
 
+import android.app.DatePickerDialog
+import android.content.Context
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.warranty.warrantyproject.databinding.FragmentAddBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddFragment : Fragment() {
 
@@ -26,10 +34,42 @@ class AddFragment : Fragment() {
             goBackHome(it)
         }
 
+        val dateOfPurchaseInputLayout = binding.productDateOfPurchase
+        val dateOfPurchaseEditText = binding.productDateOfPurchaseEditText
+
+        // Attacher un sélecteur de date à dateOfPurchaseInputLayout et dateOfPurchaseEditText
+        attachDatePickerToTextInput(dateOfPurchaseInputLayout, dateOfPurchaseEditText, requireContext())
+
+        // Trouver les TextInputLayout et TextInputEditText pour la date de garantie maximale
+        val maxGuaranteeDateInputLayout = binding.productMaxGuaranteeDate
+        val maxGuaranteeDateEditText = binding.productMaxGuaranteeDateEditText
+
+        // Attacher un sélecteur de date à maxGuaranteeDateInputLayout et maxGuaranteeDateEditText
+        attachDatePickerToTextInput(maxGuaranteeDateInputLayout, maxGuaranteeDateEditText, requireContext())
+
         return binding.root
+
     }
 
     fun goBackHome(view:View) {
         view.findNavController().navigate(R.id.action_addFragment_to_homeFragment)
+    }
+    fun attachDatePickerToTextInput(textInputLayout: TextInputLayout, textInputEditText: TextInputEditText, context: Context) {
+        val datePickerDialog = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            val calendar = Calendar.getInstance().apply {
+                set(year, monthOfYear, dayOfMonth)
+            }
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            textInputEditText.setText(dateFormat.format(calendar.time))
+        }
+        textInputEditText.inputType = InputType.TYPE_NULL
+        textInputEditText.isClickable = true
+        textInputEditText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                val calendar = Calendar.getInstance()
+                DatePickerDialog(context, datePickerDialog, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
+        textInputLayout.isHintEnabled = false
     }
 }
