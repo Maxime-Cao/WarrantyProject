88e73facc5d4a01e0f22eedce18ce66c4616aa95
Tom C <31369826+tomcauf@users.noreply.github.com>
2023-03-08 15:27:36 +0100

    Add RecyclerView

--- app/src/main/java/com/warranty/warrantyproject/HomeFragment.kt
+++ app/src/main/java/com/warranty/warrantyproject/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.warranty.warrantyproject
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,11 +8,66 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import com.warranty.warrantyproject.databinding.FragmentHomeBinding
+import com.warranty.warrantyproject.domains.Notification
+import com.warranty.warrantyproject.domains.NotificationPeriod
+import com.warranty.warrantyproject.domains.Warranty
+import com.warranty.warrantyproject.domains.WarrantyCover
+import java.sql.Date
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding : FragmentHomeBinding
 
+    private val myObjectList = listOf(
+        Warranty(
+            1001,
+            Date(2002,3,30),
+            Date(2003, 10, 30),
+            "not",
+            Notification(
+                false,
+                NotificationPeriod.ONE_YEAR
+            ),
+            WarrantyCover(
+                "not",
+                "Samsung",
+                "A phone cost 1001$",
+                "Médiamarkt"
+            )
+        ),
+        Warranty(
+            1111,
+            Date(2005,3,30),
+            Date(2007, 10, 30),
+            "not",
+            Notification(
+                false,
+                NotificationPeriod.ONE_MONTH
+            ),
+            WarrantyCover(
+                "not",
+                "iPhone",
+                "A phone cost 1111$",
+                "Apple Store"
+            )
+        )
+    )
+    /*private val myObjectList = listOf(
+        WarrantyCover(
+            1,
+        "not",
+        "Samsung",
+        "A phone cost 1001$",
+        "Médiamarkt"
+    ), WarrantyCover(
+            2,
+        "not",
+        "iPhone",
+        "A phone cost 1111$",
+        "Apple Store"
+    ) )*/
+
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,9 +77,7 @@ class HomeFragment : Fragment() {
         binding.addCompHomeScreen.setOnClickListener {
             it.findNavController().navigate(R.id.action_homeFragment_to_addFragment)
         }
-
-        // Ajouter lien de home vers look quand recyclerview ok
-
+        binding.recyclerView.adapter = MyAdapter(myObjectList.map{ it.warrantyCover })
         return binding.root
     }
 }
